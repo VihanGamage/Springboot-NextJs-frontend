60e88050c26282011fcdc005acc6530c81a57f01
VihanGamage <vihanchandula@gmail.com>
2025-06-19 22:45:35 +0530

    login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+"use client"
+
+import Login from "@/app/login/page";
+import Register from "@/app/register/page";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="">
-      
 
-    </div>
+  const [login, setLogin] = useState(true);
+
+  return (
+    <>
+      {login && <Login />}
+      {!login && <Register />}
+
+      {login && (
+        <div className="w-80 mx-auto mt-16 flex flex-col items-center space-y-4">
+          <Label>Not a User? Register now</Label>
+          <Button
+            className="cursor-pointer"
+            onClick={ () => setLogin(false)}
+          >
+            Register
+          </Button>
+        </div>
+      )}
+    </>
   );
 }
---
+++ src/app/register/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+const api =
+  process.env.NEXT_PUBLIC_API_URL ??
+  "https://ecommerce-store-vihan-bkeqaqfhc2czd7gy.southindia-01.azurewebsites.net";
+
+const FormSchema = z.object({
+  userName: z.string().min(1, "userName is required"),
+  password: z.string().min(1, "password is required"),
+});
+
+type FormValues = z.infer<typeof FormSchema>;
+
+export default function Register() {
+  const router = useRouter();
+
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      userName: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(data: FormValues) {
+    console.log(data);
+    const res = await fetch(`${api}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userName: data.userName,
+        password: data.password,
+      }),
+    });
+    if (!res.ok) {
+      console.log(`${res.status} - ${res.text}`);
+      toast.error("register failed");
+    } else {
+      console.log(`success - ${res.text}`);
+      const result = await res.json();
+      toast.success("register Successful");
+
+      localStorage.setItem("token", result.token);
+      router.push("/login");
+    }
+  }
+
+  return (
+    <>
+      <div className="border border-gray-400 rounded-lg p-6 mt-40 w-80 mx-auto">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 flex flex-col items-center"
+          >
+            <FormField
+              control={form.control}
+              name="userName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>User Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="User Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="cursor-pointer" type="submit">
+              Register
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </>
+  );
+}
